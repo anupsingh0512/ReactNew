f391b142423aa36c7521ce600c55d3b40c355c1d
Anup Singh <anup.10745342@ltimindtree.com>
2025-01-28 18:15:13 +0530

    Created React Element using JSX

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
    import ReactDOM from "react-dom/client";
 
   
+   const heading = React.createElement("h1",{id:"heading"},"Namaste React 👌");
+
+   console.log(heading);
+
+   const jsxheading = <h1 id="heading" >Namaste React using JSX</h1>
+
+   console.log(jsxheading);
+
+   const root = ReactDOM.createRoot(document.getElementById("root"));
+
+   root.render(jsxheading);
