1f51d88f4709448552cf9dd39c6e991eaf6c2c13
Anup Singh <anup.10745342@ltimindtree.com>
2025-01-28 16:28:02 +0530

    Normalize line endings

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
         
+   import React from "react";
+   import ReactDOM from "react-dom/client";
 
-   const parent = React.createElement (
-                    "div",
-                    {id:"parent"},
+   const parent = React.createElement ("div",{id:"parent"},
+                   [React.createElement
+                    ("div",
+                        {id:"child1"},
+                        [React.createElement("h1",{},"I am in h1 tag"),
+                         React.createElement("h2",{},"I m in h2 tag")
+                    ]),
                     React.createElement
-                    (
-                        "div",
-                        {id:"child"},
-                        [React.createElement("h1",{},"I am in h1 tag"),React.createElement("h2",{},"I m in h2 tag")]
-                    ),
-                    React.createElement
-                    (
-                        "div",
-                        {id:"child"},
-                        [React.createElement("h1",{},"I am in h1 tag"),React.createElement("h2",{},"I m in h2 tag")]
-                    ));
+                    ("div",
+                          {id:"child2"},
+                        [React.createElement("h1",{},"I am in h1 tag"),
+                         React.createElement("h2",{},"I m in h2 tag")
+                        ])
+        ]);
 
    console.log(parent);
 
