adef120eafebd9bdaa613240cc7420b2c99ed9bf
Anup Singh <anup.10745342@ltimindtree.com>
2025-02-18 15:15:13 +0530

    React basics

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
    import React from "react";
    import ReactDOM from "react-dom/client";
 
-  
-   
-   // React Element
-   const jsxheading = 
-   (<h1 className="head">
-        Namaste React using JSX
-    </h1>)
+   // React Component
+    const Title = () => (
+        <h1 className="head" tabIndex="5">
+                Namaste React using JSX 👌
+        </h1>
+    );
+
+    //React Functional Component 
+    const HeadingComponent = () => (
+        <div id="container">
+                <Title/>
+          <h1 className="heading"> Namaste React Functional Component </h1>      
+        </div>
+    );
 
    const root = ReactDOM.createRoot(document.getElementById("root"));
-
-   root.render(jsxheading);
+   root.render(<HeadingComponent />);
