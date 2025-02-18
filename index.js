f006ca0c1a219415dcc32753aece97c86a7f6562
Anup Singh <anup.10745342@ltimindtree.com>
2025-02-18 18:37:01 +0530

    Header Componet Updated

--- index.js
+++ index.js
@@ -2,20 +2,32 @@
    import React from "react";
    import ReactDOM from "react-dom/client";
 
-   // React Component
-    const Title = () => (
-        <h1 className="head" tabIndex="5">
-                Namaste React using JSX 👌
-        </h1>
-    );
-
-    //React Functional Component 
-    const HeadingComponent = () => (
-        <div id="container">
-                <Title/>
-          <h1 className="heading"> Namaste React Functional Component </h1>      
-        </div>
-    );
+   
+   const Header = () => {
+        return (
+                <div className="header">
+                  <div className="logo-container">
+                    <img className="logo" src="https://img.freepik.com/free-vector/ecofood-logo-template_1195-33.jpg?t=st=1739872710~exp=1739876310~hmac=2b5f0e786bf94bd5e347ea2637db9a45700920d3b6688949e477ae7e31c19079&w=740"  />
+                  </div>
+                  <div className="nav-items">
+                    <ul>
+                        <li> Home</li>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                        <li>Cart </li>
+                    </ul>
+                  </div>
+                </div>
+        )
+    }
+   
+    const AppLayout = () => {
+        return (
+                <div className="app">
+                 <Header />
+                </div>
+        )
+   }
 
    const root = ReactDOM.createRoot(document.getElementById("root"));
-   root.render(<HeadingComponent />);
+   root.render(<AppLayout />);
